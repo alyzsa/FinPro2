e38b8849ab6a65772d4b714664b7c63fe7523af7
Alysa <127311464+alyzsa@users.noreply.github.com>
2023-11-24 20:33:38 +0700

    fixing port

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"github.com/alyzsa/FinPro2/database"
 	"github.com/alyzsa/FinPro2/router"
-	
+	"os"
 )
 
 func main() {
 	database.StartDB()
+
+	// Get the port from the environment variable or default to "8080"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	r := router.StartApp()
-	r.Run(":8080")
+	r.Run(":" + port)
 }
